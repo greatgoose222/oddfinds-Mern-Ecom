6aa707d28a958ef8edc5cbe71d11eae2b0db30f1
Mukesh Kumar <mk981447107@gmail.com>
2026-02-20 22:42:34 +0530

    logot fix

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -88,8 +88,12 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
     try {
         res.clearCookie("token", {
-            path: "/",
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            path: "/"
         });
+
         res.status(200).json({ message: "User logged out successfully" });
     } catch (error) {
         console.log(error);
