38259cbde1f83f8cd6157894c30b512963e14554
Mukesh Kumar <mk981447107@gmail.com>
2026-02-21 18:25:22 +0530

    localhost proxy setup ,vite config &api.js changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.PROD
+        ? import.meta.env.VITE_API_URL   // production
+        : "",                            // development (use proxy)
     withCredentials: true,
 });
 
 export default api;
+
+
+
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL,
+//     withCredentials: true,
+// });
+
+// export default api;
