6c034a83a4222de64f5b778a32f46eaa953062bd
Mukesh Kumar <mk981447107@gmail.com>
2026-06-03 00:49:03 +0530

    css chnages in admin product page

--- client/src/pages/admin/AdminProducts.jsx
+++ client/src/pages/admin/AdminProducts.jsx
@@ -29,6 +29,19 @@ const AdminProducts = () => {
         getProducts();
     }, []);
 
+    const handleDelete = async (id) => {
+        try {
+            const response = await api.delete(`/api/product/${id}`, {
+                withCredentials: true
+            })
+            setProducts(prev =>
+                prev.filter(p => String(p._id) !== String(id))
+            );
+        } catch (error) {
+            setError("Something went wrong"); console.log(error)
+        }
+    }
+
 
     // if (loading) return <p>Loading...</p>;
     if (loading) return <ProductGridSkeleton num={5} />;
@@ -42,11 +55,13 @@ const AdminProducts = () => {
             <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-6 ">
 
                 {products.map((product) => {
-                    return <div key={product._id}>
+                    return <div key={product._id} className="relative">
                         <Link to={`/product/${product.slug}`}>
                             <img src={product?.images?.featured?.url} className="rounded-lg" />
                             <p className="mt-1">{product.name}</p>
                         </Link>
+                        <button className=" absolute top-2 left-2 cursor-pointer hover:bg-red-400 bg-red-500 px-2 rounded text-sm text-amber-50"
+                            onClick={() => handleDelete(product._id)}>Delete</button>
                         <div className="flex gap-1">
                             <p className="font-medium">₹{product.sellingPrice}</p>
                             <p className="text-gray-400 line-through">₹{product.price}</p>
@@ -61,4 +76,3 @@ const AdminProducts = () => {
 };
 
 export default AdminProducts;
-
